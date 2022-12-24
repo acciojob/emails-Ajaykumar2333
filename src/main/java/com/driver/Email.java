cde75234a813ca3c967c397e3875788ae87ef921
ajaya <vodlaajay234@gmail.com>
2022-12-24 22:46:20 +0530

    messge2

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -1,5 +1,8 @@
 package com.driver;
 
+import java.util.Arrays;
+import java.util.HashSet;
+
 public class Email {
 
     private String emailId;
@@ -11,55 +14,46 @@ public class Email {
     }
 
     public String getEmailId() {
+
         return emailId;
     }
 
     public String getPassword() {
+
         return password;
     }
 
-        public void changePassword(String oldPassword, String newPassword){
-            boolean flag = false;
-            if(newPassword.length() <= 8) {
-                flag = crctPass(newPassword);
+    public void changePassword(String oldPassword, String newPassword){
+        //Change password only if the oldPassword is equal to current password and the new password meets all of the following:
+        // 1. It contains at least 8 characters
+        // 2. It contains at least one uppercase letter
+        // 3. It contains at least one lowercase letter
+        // 4. It contains at least one digit
+        // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
+        if(password.equals(oldPassword)){
+            boolean haveLower=false;
+            boolean haveUpper=false;
+            boolean haveDigit=false;
+            boolean havespl=false;
+//           HashSet<Character>set=new HashSet<>(Arrays.asList('!', '@', '#', '$', '%', '^', '&','*', '(', ')', '-', '+'));
+            for(char i: newPassword.toCharArray()){
+                if(Character.isLowerCase(i)){
+                    haveLower=true;
+                }
+                else if(Character.isUpperCase(i)){
+                    haveUpper=true;
+                }
+                else if(Character.isDigit(i)){
+                    haveDigit=true;
+                }
+                else {
+                    havespl=true;
+                }
             }
-          else{
-                System.out.println("your password is not meets all criteria");
-
-            }
-
-          if(flag == true){
-              if(oldPassword.equals(this.password)){
-                  password = newPassword;
-          }
-       }
-    }
-
-    public static boolean crctPass(String password){
-        boolean Num = false,caps = false,low = false,spec = false;
-        char c;
-        for(int i = 0;i<password.length();i++){
-            c = password.charAt(i);
-            if(Character.isDigit(c)){
-                Num = true;
-            }
-            else if (Character.isUpperCase(c)){
-                caps = true;
-            }
-            else if  (Character.isLowerCase(c)){
-                low = true;
-            }else{
-                spec = true;
-            }
-            if(Num && caps && low && spec){
-                return true;
+            if((havespl == true && haveDigit == true && haveLower == true && haveUpper ==true) && newPassword.length()>=8){
+                password=newPassword;
             }
 
         }
-        return false;
     }
-
-
 }
-
-
